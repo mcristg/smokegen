f64adb93d93d6ab155c1aad285392291bf598ce0
mcrist <mcristernag@gmail.com>
2022-05-27 13:31:36 -0700

    Problem with Visual Studio 2022 version 17.2+. Linker undefined symbol

--- main.cpp
+++ main.cpp
@@ -16,6 +16,11 @@
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
+#if _MSC_VER >= 1932 // Visual Studio 2022 version 17.2+
+#    pragma comment(linker, "/alternatename:__imp___std_init_once_complete=__imp_InitOnceComplete")
+#    pragma comment(linker, "/alternatename:__imp___std_init_once_begin_initialize=__imp_InitOnceBeginInitialize")
+#endif
+
 #include <QCoreApplication>
 #include <QList>
 #include <QDir>
